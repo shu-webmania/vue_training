2e9fcd696a46f4302ecfffadcc9a1f4489e42ce4
shuwebdesign <littlecub114mt@gmail.com>
2022-02-06 19:37:11 +0900

    v-on

--- script.js
+++ script.js
@@ -2,25 +2,11 @@ new Vue({
   el: "#app",
   data: {
     number: 0,
-    x: 0,
-    y: 0,
+    // event: "click",
   },
   methods: {
-    countUp: function (times) {
-      this.number += 1 * times;
-    },
-    changeMousePositon: function (divideNumber, event) {
-      this.x = event.clientX / divideNumber;
-      this.y = event.clientY / divideNumber;
-    },
-    // noEvent: function (event) {
-    //   event.stopPropagation();
-    // },
-    // noEvent: function (event) {
-    //   event.preventDefault();
-    // },
-    myAlert: function () {
-      alert("アラート");
+    countUp: function () {
+      this.number += 1;
     },
   },
 });
