b5086dcd43b812fb8687d645e7f01329c1fe83fc
shuwebdesign <littlecub114mt@gmail.com>
2022-02-06 19:42:11 +0900

    v-model

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 new Vue({
   el: "#app",
   data: {
-    number: 0,
-    // event: "click",
-  },
-  methods: {
-    countUp: function () {
-      this.number += 1;
-    },
+    message: "こんにちは",
   },
 });
