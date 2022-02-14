bff61bd24e34369b4f2d99fbac40b0eefedd0528
shuwebdesign <littlecub114mt@gmail.com>
2022-02-15 08:11:38 +0900

    watch

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 new Vue({
   el: "#app",
   data: {
-    message: "こんにちは",
+    counter: 0,
+  },
+  computed: {
+    lessThanThree: function () {
+      return this.counter > 3 ? "3以上" : "3以下";
+    },
+  },
+  watch: {
+    counter: function () {
+      var vm = this;
+      setTimeout(function () {
+        vm.counter = 0;
+      }, 3000);
+    },
   },
 });
