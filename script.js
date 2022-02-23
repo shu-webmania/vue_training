62e08a9309be65ace4c8ec75d2ac38ab4ba037ed
shuwebdesign <littlecub114mt@gmail.com>
2022-02-24 07:15:39 +0900

    v-for

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 new Vue({
   el: "#app",
   data: {
-    ok: true,
+    fruits: ["りんご", "バナナ", "ぶどう"],
+    object: {
+      firstName: "太郎",
+      lastName: "田中",
+      age: 21,
+    },
   },
 });
